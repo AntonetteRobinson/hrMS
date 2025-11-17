dcc557a4f62697b6e8d50d297b672d9ddafec5cc
Lamar Shaw <lamarshaw@Lamars-MacBook-Air.local>
2025-11-17 14:29:06 -0500

    java file

---
+++ Mileage.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Mileage{
+    /**Testing 1,2,3 */
+    public static void main(String[] args){
+        int miles;
+        double gallons, mpg;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter miles and gallons: ");
+        miles = s.nextInt();
+        gallons = s.nextDouble();
+        mpg = miles / gallons;
+
+        System.out.println("Miles" + "per" + "Gallon:" + mpg);
+    }
+}
